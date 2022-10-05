f00c38a20bca202633823d06c1177e8adfa5a46e
sorath23 <sorath@ucsc.edu>
2022-10-05 15:51:56 -0700

    pushedits2

---
+++ lab2/js/lab.js
@@ -0,0 +1,16 @@
+h1 {
+  color: DodgerBlue;
+}
+
+
+body {
+  background-image: stars.jpeg;
+  font-family: Verdana;
+  background-color: darkgray;
+  padding: 25px;
+  font-size: 120%;
+}
+
+p {
+  color: red;
+}

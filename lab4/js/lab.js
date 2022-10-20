eac14fa7d263d97057df53158e3d38acf091aa3c
sorath23 <sorath@ucsc.edu>
2022-10-20 08:18:50 -0700

    lab5pushes

---
+++ lab4/js/lab.js
@@ -0,0 +1,10 @@
+<pre><code>
+//Open document in Google Docs
+  //Fill document with designated information
+  //Access Google Doc toolbar
+    //Click File
+    //Click Print
+      //Select printer destination
+      //Verify printer settings
+    //Click Print
+</pre></code>
---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/**
+* Author: Anika, Ellery
+* Created 18 October
+* License: Public Domain
+*/
+
+// Define Variables
+var make = "Ferrari";
+var model = "250 Spyder";
+var color = "Black";
+var year = 1957;
+var ownIt = 1;
+
+// calculate
+var age = 2022 - year;
+
+// output
+document.writeln("make: " + make + "<br>");
+document.writeln("model: " + model + "<br>");
+document.writeln("color: " + color + "<br>");
+document.writeln("year: " + year + "<br>");
+document.writeln("age: " + age + " years<br>");
+document.writeln("ownIt: " + ownIt + "<br>");

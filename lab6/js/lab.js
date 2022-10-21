c5d1a93055864bd613dd512130a14625b1aecbcf
sorath23 <sorath@ucsc.edu>
2022-10-20 18:24:04 -0700

    lab6 edits

---
+++ lab6/js/lab.js
@@ -0,0 +1,25 @@
+/**
+* Author: Anika, Ellery
+* Created 18 October
+* License: Public Domain
+*/
+
+// Define Variables
+var myTransport = ["Ferrari 250 Spyder", "bike", "rides from friends", "Lime Scooter"];
+
+console.log ("My Main Ride:", myMainRide);
+var myMainRide = {
+    make : "Ferrari",
+    model : "250 Spyder",
+    color : "Black",
+    year : 1957,
+    ownIt : 1,
+    age : function() {
+      return 2022 - age;
+    }
+}
+
+// output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br");
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");

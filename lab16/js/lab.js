9699690ed028ebbd8debd5786ede63a74dc5b73f
sorath23 <sorath@ucsc.edu>
2022-11-30 12:19:34 -0800

    push

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -4,6 +4,10 @@
  * License: Public Domain
  */
 
+ var start = "https://xkcd.com/";
+  var end = "/info.0.json";
+  var num = "";
+  var largest = 2704;
 
  var URL = "https://xkcd.com/info.0.json";
 
@@ -29,21 +33,22 @@
    }
    var ourURL = URLpre + numStr + URLpost;
    console.log("Our new URL:", ourURL);
-   // get data via ajax from numbersapi
-   // Using the core $.ajax() method
+
    $.ajax({
-       // The URL for the request (ENDPOINT)
-       url: ourURL,
-       // Whether this is a POST or GET request
-       type: "GET",
+     url: newUrl,
+     type: "GET",
+     dataType : "json",
+     success: function(comicObj) {
+         console.log(comicObj);
+     },
+     error: function (jqXHR, textStatus, errorThrown) {
+         console.log("Error:", textStatus, errorThrown);
+     }
    })
-   // If the request succeeds
+   // If succeed
    .done(function(data) {
        // console.log(data);
        var imageUrl = data.img;
-       // we use .replace(/"/g, '\\"') after the text strings because
-       // sometimes there are single and double quotes in the text
-       // that confuses the html
        var title = data.title;
        console.log("orig title:", title);
        title = make_safe(title);
@@ -56,26 +61,18 @@
        var html = `<div id="imageblock">
            <h2>${title}</h2>
            <img src="${imageUrl}" title="${alt}"><br>
-           <button id="prev">Previous</button><button id="next">Next</button>
+           <button id="prev">Back</button><button id="next">Next</button>
          </div>`
-       // console.log("My new html: \n", html);
+
        $("#output").html(html);
 
-       // add event listener to new prev button
        $("#prev").click(function(){
          getComic(comicNum - 1);
        });
-       // add event listener to new next button
        $("#next").click(function(){
          getComic(comicNum + 1);
        });
    })
-   .fail(function(){
-     console.log("^^ Please ignore this error. It's okay.");
-     console.log("Have a okay day! :-)");
-   })
-
- }
 
  // start things off
  getComic();

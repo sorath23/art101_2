68c4c3c87343a599baf9243272848bb9615dec67
sorath23 <sorath@ucsc.edu>
2022-11-30 12:45:32 -0800

    d

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -7,16 +7,10 @@
 
 var URL = "https://xkcd.com/info.0.json";
 
-// https://xkcd.com/json.html
-// endpoint https://xkcd.com/456/info.0.json
 var URLpre = "https://xkcd.com/";
 var URLpost = "info.0.json";
 
-// Endpoint format: http://xkcd.com/614/info.0.json
 
-// a helper function to turn single and double quotes into
-// html symbols so they don't confuse html tags
-//
 function make_safe(str) {
   return str.replace(/'/g, '&apos;').replace(/"/g, '&quot;');
 }
@@ -42,13 +36,9 @@ function getComic(num) {
           console.log("Error:", textStatus, errorThrown);
       }
   })
-  // If the request succeeds
   .done(function(data) {
-      // console.log(data);
       var imageUrl = data.img;
-      // we use .replace(/"/g, '\\"') after the text strings because
-      // sometimes there are single and double quotes in the text
-      // that confuses the html
+
       var title = data.title;
       console.log("orig title:", title);
       title = make_safe(title);
@@ -66,12 +56,11 @@ function getComic(num) {
       $("#output").append("<p>" + comicObj.alt + "<p>");
       $("#output").html(html);
 
-      // add event listener to new prev button
       $("#prev").click(function(){
         getComic(comicNum - 1);
         getAndPutData(num);
       });
-      // add event listener to new next button
+
       $("#next").click(function(){
         getComic(comicNum + 1);
         getAndPutData(num);
@@ -79,5 +68,4 @@ function getComic(num) {
   })
 }
 
-// start things off
 getComic();

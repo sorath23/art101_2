d9b807719916d545f3eec44f6c1c56daebfce04f
sorath23 <sorath@ucsc.edu>
2022-11-30 13:27:20 -0800

    g

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -50,23 +50,23 @@ function getComic(num) {
       console.log("safe alt:", alt);
       var comicNum = data.num;
       var html = `<div id="imageblock">
-          <h2>${title}</h2>
-          <img src="${imageUrl}" title="${alt}"><br>
-          <button id="prev">Previous</button><button id="next">Next</button>
-        </div>'
+     <h2>${title}</h2>
+     <img src="${imageUrl}" title="${alt}"><br>
+     <button id="prev">Previous</button><button id="next">Next</button>
+   </div>`
+ // console.log("My new html: \n", html);
+ $("#output").html(html);
 
-      $("#output").html(html);
-
-      $("#prev").click(function(){
-        getComic(comicNum - 1);
-        getAndPutData(num);
-      });
-
-      $("#next").click(function(){
-        getComic(comicNum + 1);
-        getAndPutData(num);
-      });
-  })
+ // add event listener to new prev button
+ $("#prev").click(function(){
+   getComic(comicNum - 1);
+ });
+ // add event listener to new next button
+ $("#next").click(function(){
+   getComic(comicNum + 1);
+ });
+})
 }
 
+// start things off
 getComic();
